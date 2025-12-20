7af79249e81b0bc2915eb6046c72a12019804317
somadina94 <williamsonuaguluchi94@gmail.com>
2025-12-20 13:55:56 +0100

    Feature/Test_Notification5

--- src/workers/notification-worker.ts
+++ src/workers/notification-worker.ts
@@ -2,20 +2,40 @@ import { Worker } from "bullmq";
 import { redis } from "../lib/redis.js";
 import Notification from "../models/notificationModel.js";
 import { NotificationService } from "../services/notification-service.js";
+import mongoose from "mongoose";
+
+const dbUrl = process.env.DATABASE!;
+const dbPassword = process.env.DATABASE_PASSWORD!;
+
+const DB = dbUrl.replace("<password>", dbPassword);
+
+const connectDB = async () => {
+  const database = await mongoose.connect(DB);
+  if (database.STATES.connected === 1)
+    console.log("DB connected successfully!!!");
+};
+
+connectDB();
 
 export const notificationWorker = new Worker(
   "notifications",
   async (job) => {
-    const { notificationId } = job.data;
+    try {
+      const { notificationId } = job.data;
+      console.log(notificationId);
 
-    const notification = await Notification.findById(notificationId);
+      const notification = await Notification.findById(notificationId);
 
-    if (!notification) return;
+      if (!notification) return;
 
-    // Idempotency guard (VERY important)
-    if (notification.sentAt) return;
+      // Idempotency guard (VERY important)
+      if (notification.sentAt) return;
 
-    await NotificationService.send(notification);
+      await NotificationService.send(notification);
+      // console.log("✅ Sent job", job.id);
+    } catch (error) {
+      // console.error("❌ Send failed", error);
+    }
   },
   {
     connection: redis,
